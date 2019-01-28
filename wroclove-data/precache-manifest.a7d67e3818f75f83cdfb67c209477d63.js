aa1be34e45102fdc20587c7dcd5d3f541e97d2a3
PGS-SOFT\amortka <Di477363>
2019-01-28 10:16:16 +0100

    wip wroclove

---
+++ wroclove-data/precache-manifest.a7d67e3818f75f83cdfb67c209477d63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e576ccabd947e20372c",
+    "url": "/wroclove-data/static/js/runtime~main.5e576cca.js"
+  },
+  {
+    "revision": "1b3db9b31c2824817c8d",
+    "url": "/wroclove-data/static/js/main.1b3db9b3.chunk.js"
+  },
+  {
+    "revision": "38f9ae1be398e95b9158",
+    "url": "/wroclove-data/static/js/1.38f9ae1b.chunk.js"
+  },
+  {
+    "revision": "1b3db9b31c2824817c8d",
+    "url": "/wroclove-data/static/css/main.6d15da63.chunk.css"
+  },
+  {
+    "revision": "c5724417a8f179999d82221fc3d23f50",
+    "url": "/wroclove-data/index.html"
+  }
+];
