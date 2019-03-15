ff733d90a75609a4a7717c94322aece5ed433960
PGS-SOFT\amortka <Di477363>
2019-03-15 07:33:11 +0100

    wheels

---
+++ wheels/precache-manifest.a306bde23be1653a498988acd8a07391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wheels/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2951ca1cbd50d6e81328",
+    "url": "/wheels/static/js/runtime~main.8e3ec23f.js"
+  },
+  {
+    "revision": "89332fd6801695cec705",
+    "url": "/wheels/static/js/main.69501d66.chunk.js"
+  },
+  {
+    "revision": "cf6c560f9de62942c638",
+    "url": "/wheels/static/js/2.a968de20.chunk.js"
+  },
+  {
+    "revision": "89332fd6801695cec705",
+    "url": "/wheels/static/css/main.6df6dd13.chunk.css"
+  },
+  {
+    "revision": "21d5411f9d9dd879082f3670c7d03d7e",
+    "url": "/wheels/index.html"
+  }
+];
